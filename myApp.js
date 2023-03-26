c1d2f36348be88bfaa2e00be26cede6c155d6c61
rrsbrg <rosebergrobert@gmail.com>
2023-03-26 14:35:51 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,27 +1,32 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
+
+/*
+function respond(req, res) {
+  res.send("Hello Express");
+}
+
+app.get("/", respond);
 
 
 
 
+function respond(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+}
+
+app.get("/", respond);
+app.use("/public", express.static(__dirname + "/public"));
+*/
 
 
+function respondJSON(req, res) {
+  res.json({"message": "Hello json"});
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", respondJSON);
 
 
 
